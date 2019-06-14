a5fe11baf2e02bfce446d9637e3529540632eb77
Lawrence <lawrencekkyalo@gmail.com>
2019-06-14 11:17:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
+        var altitude = position.coords.altitude;
         
 
         // work with this information however you'd like!

c0e32aaefadcd239166bc0a06b057b0c7e60c16b
Lawrence <lawrencekkyalo@gmail.com>
2019-06-14 11:18:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
         var accuracy = position.coords.accuracy;
         var altitudeAccuracy = position.coords.altitudeAccuracy;
         var heading = position.coords.height;
+        var speed = position.coords.speed;
         
 
         // work with this information however you'd like!

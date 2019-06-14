ad5f1ead23e80bce67f7758e3a8d96ead8fbe6a2
Lawrence <lawrencekkyalo@gmail.com>
2019-06-14 11:16:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
  navigator.geolocation.getCurrentPosition(function(position) {
 
         var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
         
 
         // work with this information however you'd like!

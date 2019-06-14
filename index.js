4dcbdb5ffa59cc25702a5db7561303989ae39e05
Lawrence <lawrencekkyalo@gmail.com>
2019-06-14 11:17:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
         var altitude = position.coords.altitude;
+        var accuracy = position.coords.accuracy;
         
-
         // work with this information however you'd like!
     });
